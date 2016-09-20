94d55c9e35e945db42d0950418201e9cffe0b523
Luiz Picolo ツ <luizpicolo@gmail.com>
2016-09-20 07:48:33 -0400

    Update install_ruby.sh

--- install_ruby_and_rvm/install_ruby.sh
+++ install_ruby_and_rvm/install_ruby.sh
@@ -4,8 +4,8 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 curl -L https://get.rvm.io | bash -s stable --ruby &&
 source ~/.rvm/scripts/rvm &&
 rvm requirements &&
-rvm install 2.3.1 &&
-rvm use 2.3.1 --default &&
+rvm install $(VERSION_RUBY) &&
+rvm use $(VERSION_RUBY) --default &&
 rvm rubygems current &&
 ssh-keygen &&
 cat ~/.ssh/id_rsa.pub
